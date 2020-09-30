906cc62d8108ae4ee371b2a269d51a4a6c2a5f05
Zeyneb <46132459+Zeynebb@users.noreply.github.com>
2020-09-30 16:24:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace KelimeCumleAyırma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Location = new System.Drawing.Point(11, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(227, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(11, 380);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(624, 147);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(244, 221);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(391, 147);
+            this.listBox2.TabIndex = 2;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(245, 30);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(390, 173);
+            this.listBox3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(227, 231);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Metni Giriniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(11, 365);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cümleler:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(242, 206);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kelimeler:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(242, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Heceler:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button2.Location = new System.Drawing.Point(11, 320);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(227, 43);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "SIFIRLA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(645, 538);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelimeCumleAyırma
+{
+    public partial class Form1 : Form
+    {
+        //Proje Sahibi: Zeyneb Eda Yılmaz
+        //https://github.com/Zeynebb
+        public Form1()
+        {InitializeComponent();}
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+        Char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü','A',
+        'E','I','İ','O','Ö','U','Ü'};
+        Char[] sessizHarfler = { 'b', 'c', 'ç', 'd', 'f', 'g', 'ğ', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 'ş',
+            't', 'v', 'y', 'z', 'B', 'C', 'Ç', 'D', 'F', 'G', 'Ğ', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'R',
+            'S', 'Ş', 'T', 'V', 'Y', 'Z' };
+        Char[] karakter = { ' ', '!', '?', '/', '.', ',', ':', ';'};
+        int heceSayisi = 0;
+        String hece = "";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            heceSayisi = 0;
+            String metin = textBox1.Text;
+            String[] cumleler = metin.Split('.', '!', '?');
+            String[] kelimeler = metin.Split(' ', ',', ':', ';','?', '.', '!');
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                listBox2.Items.Add("Kelime " + (i + 1) + ": " + kelimeler[i]);
+                char[] harfler = kelimeler[i].ToCharArray();
+                heceAyir(harfler, kelimeler[i]);
+            }
+            for (int i = 0; i < cumleler.Length - 1; i++)
+            {
+                listBox1.Items.Add("Cümle " + (i + 1) + ": " + cumleler[i]);
+            }
+        }
+        public bool SesliHarfMi(char karakter)
+        {
+            bool sonuc = false;
+            for (int i = 0; i < sesliHarfler.Length; i++)
+            {
+                if (karakter == sesliHarfler[i])
+                {
+                    sonuc = true;
+                    break;
+                }
+                else
+                {
+                    sonuc = false;
+                }
+            }
+            return sonuc;
+        }
+        public bool SessizHarfMi(char karakter)
+        {
+            bool sonuc = false;
+            for (int i = 0; i < sessizHarfler.Length; i++)
+            {
+                if (karakter == sessizHarfler[i])
+                {
+                    sonuc = true;
+                }
+                else
+                {
+                    sonuc = false;
+                }
+            }
+            return sonuc;
+        }
+        public int SozKacHeceli(String soz)
+        {
+            char[] karakterler = soz.ToCharArray();
+            for (int s = 0; s < karakterler.Length; s++)
+            {
+                SesliHarfMi(karakterler[s]);
+            }
+            listBox3.Items.Add("hece sayısı: " + heceSayisi);
+            return heceSayisi;
+        }
+        public bool boslukMu(char karakterler)
+        {
+            for (int i = 0; i < karakter.Length; i++)
+            {
+                if (karakter[i] == karakterler) ;
+            }
+            return true;
+        }
+        public void heceAyir(char[] harfler, String kelime)
+        {
+            for (int i = 0; i < harfler.Length; i++)
+            {
+                if (SesliHarfMi(harfler[i]) == true)//ilk harf sesli
+                {
+                    if (i + 1 >= harfler.Length)
+                    {
+                        listBox3.Items.Add("Hecelenmiş Kelime: " + hece+ kelime.Substring(i));
+                        hece = "";
+                        break;
+                    }
+                    else
+                    {
+                        if (SesliHarfMi(harfler[i + 1]) == false)//ikinci harf sessiz
+                        {
+                            if (i + 2 >= harfler.Length)
+                            {
+                                listBox3.Items.Add("Hecelenmiş Kelime: " + hece+kelime.Substring(i));
+                                hece = "";
+                                break;
+                            }
+                            else
+                            {
+                                if (SesliHarfMi(harfler[i + 2]) == false)//3.harf sessiz
+                                {
+                                    if (i + 3 >= harfler.Length)
+                                    {
+                                        listBox3.Items.Add("Hecelenmiş Kelime: " + hece+kelime.Substring(i));
+                                        hece = "";
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        if (SesliHarfMi(harfler[i + 3]) == false)//4.harf sessiz
+                                        {
+                                            hece=hece+kelime.Substring(i, 3) + "-";//ilk 3 harf hece
+                                            kelime = kelime.Substring(2);
+                                            harfler = kelime.ToCharArray();
+                                        }
+                                        if (SesliHarfMi(harfler[i + 3]) == true)//4.harf sesliyse 
+                                        {
+                                            hece=hece+kelime.Substring(i, 2) + "-";//ilk 2 harf hece
+                                            kelime = kelime.Substring(1);
+                                            harfler = kelime.ToCharArray();
+                                        }
+                                    }
+                                }
+                                else if (SesliHarfMi(harfler[i + 2]) == true)//3.harf sesliyse
+                                {
+                                    hece = hece + kelime.Substring(i, 1) + "-";//ilk harf hece
+                                    kelime = kelime.Substring(0);
+                                    harfler = kelime.ToCharArray();
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (SesliHarfMi(harfler[i]) == false)//ilk harf sessiz
+                {
+                    if ((i + 1) >= harfler.Length)
+                    {
+                        listBox3.Items.Add("Hecelenmiş Kelime: " + hece+ kelime.Substring(i));
+                        hece = "";
+                        break;
+                    }
+                    else
+                    {
+                        if (SesliHarfMi(harfler[i + 1]) == true)//ikinci sesli
+                        {
+                            if (i + 2 >= harfler.Length)
+                            {
+                                listBox3.Items.Add("Hecelenmiş Kelime: " + hece+ kelime.Substring(i));
+                                hece = "";
+                                break;
+                            }
+                            else
+                            {
+                                if (SesliHarfMi(harfler[i + 2]) == false)//3.harf sessiz
+                                {
+                                    if (i + 3 >= harfler.Length)
+                                    {
+                                        listBox3.Items.Add("Hecelenmiş Kelime: " + hece+ kelime.Substring(i));
+                                        hece = "";
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        if (SesliHarfMi(harfler[i + 3]) == true)//4.harf sesli
+                                        {
+                                            hece = hece + kelime.Substring(i, 2) + "-";//ilk 2 harf hece**
+                                            kelime = kelime.Substring(1);
+                                            harfler = kelime.ToCharArray();
+                                        }
+                                        else if (SesliHarfMi(harfler[i + 3]) == false)//4.sessiz
+                                        {
+                                            if (i + 4 >= harfler.Length)
+                                            {
+                                                listBox3.Items.Add("Hecelenmiş Kelime: " +hece+  kelime.Substring(i));
+                                                hece = "";
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                if (SesliHarfMi(harfler[i + 4]) == true)//5.harf sesli
+                                                {
+                                                    hece = hece + kelime.Substring(i, 3) + "-";//ilk 3 harf hece
+                                                    kelime = kelime.Substring(2);
+                                                    harfler = kelime.ToCharArray();
+                                                }
+                                                else if (SesliHarfMi(harfler[i + 4]) == false)//5.harf sessiz
+                                                {
+                                                    hece = hece + kelime.Substring(i, 4) + "-";//ilk 4 harf hece
+                                                    kelime = kelime.Substring(3);
+                                                    harfler = kelime.ToCharArray();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                else if (SesliHarfMi(harfler[i + 2]) == true)//3.harf sesli
+                                {
+                                    hece=hece+kelime.Substring(i, 2) + "-";//ilk 2 harf hece
+                                    kelime = kelime.Substring(1);
+                                    harfler = kelime.ToCharArray();
+                                }
+                            }
+                        }
+                        else if (SesliHarfMi(harfler[i + 1]) == false)//2.harf sessiz
+                        {
+                            hece = hece + kelime.Substring(i, 4) + "-";//ilk 4 harf hece
+                            kelime = kelime.Substring(3);
+                            harfler = kelime.ToCharArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
